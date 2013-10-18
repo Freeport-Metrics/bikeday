1a34680041bb3efec8a43fd8b16a0c5cf48bf4d7
Jakub Piasecki <jakub@freeportmetrics.com>
2013-10-18 19:16:38 +0200

    finding nearest station

--- js/veturilo.js
+++ js/veturilo.js
@@ -1,22 +1,44 @@
-function findLocations(callback){
+function findStations(callback) {
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8765/mockdata/veturilo.xml',
         dataType: 'xml',
         success: function (xml) {
-            var locations = [];
+            var stations = [];
             $(xml).find('place').each(function () {
                 var place = $(this);
-                var location = {
+                var station = {
                     name: place.attr('name'),
                     lat: place.attr('lat'),
                     lng: place.attr('lng'),
                     bikes: place.attr('lng'),
                     racks: place.attr('bike_racks')
                 };
-                locations.push(location);
+                stations.push(station);
             });
-            callback(locations);
+            callback(stations);
         }
     });
 }
+
+var distanceSquare = function(pointA, pointB) {
+    var x = pointA.lat - pointB.lat;
+    var y = pointA.lng - pointB.lng;
+    return (x * x) + (y * y);
+}
+
+
+
+function findNearestStation(point, stations) {
+    var nearest = null;
+    var smallestDistanceSquare;
+    $.each(stations, function (i, station) {
+        var distance = distanceSquare(point, station);
+        if ((nearest == null) || (smallestDistanceSquare > distance)) {
+            nearest = station;
+            smallestDistanceSquare = distance;
+        }
+    });
+    return nearest;
+}
+

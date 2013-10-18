1762506d4c1e4db605a0ddd4be3edfe0e1dcd666
Piotr Delinowski <piotr@freeportmetrics.com>
2013-10-18 17:42:20 +0200

    map in background, customizations to Map object, some CSS improvements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-
+$(document).ready(function () {
+    var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(52.2324, 21.0127),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+//        mapTypeControlOptions: {
+//            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+//            position: google.maps.ControlPosition.RIGHT_TOP
+//        }
+        panControl: true,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        scaleControl: true,
+        scaleControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        }
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+});

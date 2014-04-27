ed1622e5147c2bd9f70efe0fbe32d6e9053e7333
Tomek Janiszewski <janiszt@gmail.com>
2014-04-27 19:52:54 +0200

    Remove copy task for img

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    copy: {
-      main: {expand: true, cwd: 'src/', src: ['img/**'], dest: 'dist/'}
-    },
     cssmin: {
       add_banner: {
         options: {
@@ -59,7 +56,7 @@ module.exports = function(grunt) {
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'copy', 'cssmin', 'concat', 'includeSource']
+      tasks: ['jshint', 'cssmin', 'concat', 'includeSource']
     }
   });
 
@@ -69,10 +66,9 @@ grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-include-source');
-grunt.loadNpmTasks('grunt-contrib-copy');
 
 grunt.registerTask('test', ['jshint']);
 
-grunt.registerTask('default', ['jshint', 'copy', 'cssmin', 'concat', 'uglify', 'includeSource']);
+grunt.registerTask('default', ['jshint', 'cssmin', 'concat', 'uglify', 'includeSource']);
 
 };
